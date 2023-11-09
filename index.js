4772bc319f913f6307ffcf57dc49845036457c1c
Galib-23 <aagalib2323@gmail.com>
2023-11-09 17:04:58 +0600

    logout added

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true
+}));
 
 
 
@@ -99,6 +104,17 @@ async function run() {
             const user = req.body;
             console.log("user: ", user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'} )
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            })
+            .send({success: true});
+        })
+        app.post('/logout', async(req, res) =>{
+            const user = req.body;
+            console.log('Logging out..', user);
+            res.clearCookie('token', {maxAge: 0}).send({success: true})
         })
 
         app.put('/foods/:id', async (req, res) => {
