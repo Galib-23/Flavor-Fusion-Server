4c63cf2e9a5c2a0a250fd1cc01d68004be619d0c
Galib-23 <aagalib2323@gmail.com>
2023-11-09 17:08:45 +0600

    Cookie Parser added

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+
 app.use(cors({
     origin: [
         'http://localhost:5173'
     ],
     credentials: true
 }));
+app.use(express.json());
+app.use(cookieParser());
 
 
 
